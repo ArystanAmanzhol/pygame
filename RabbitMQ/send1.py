d70a73cfbd5799f40cfd43e17b572b7146919013
ArystanAmanzhol <63577686+ArystanAmanzhol@users.noreply.github.com>
2020-04-29 14:39:48 +0600

    Create send1.py

---
+++ RabbitMQ/send1.py
@@ -0,0 +1,12 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='example1')
+
+message = ' '.join(sys.argv[1:]) or "hello world..."
+channel.basic_publish(exchange='', routing_key='example1', body=message)
+
+print("Sent 'Example1'")
+channel.close()
